cad96719c79aded6d5603e7a1059b235c9330c7a
luigibolzon <51181864+luigibolzon@users.noreply.github.com>
2025-04-10 20:09:45 -0300

    criei uma selectorlist para deixar o codigo mais facil de entender

--- cypress/e2e/login-success.cy.js
+++ cypress/e2e/login-success.cy.js
@@ -1,10 +1,27 @@
 describe('Orange Tests', () => {
+
+  const selectorList = {
+      loginField: ":nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input",
+      passwordField: ":nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input",
+      loginButton: ".oxd-button",
+      sectionTopBar: ".oxd-topbar-header-breadcrumb > .oxd-text",
+      sectionTopBarText: "Dashboard",
+      loginFailAlert: ".oxd-alert"
+  }
   it('login success', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin')
-    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
-    cy.get('.oxd-button').click()
+    cy.get(selectorList.loginField).type('admin')
+    cy.get(selectorList.passwordField).type('admin123')
+    cy.get(selectorList.loginButton).click()
     cy.location('pathname').should('equal','/web/index.php/dashboard/index')
-    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard')
+    cy.get(selectorList.sectionTopBar).contains('Dashboard')
+  })
+
+  it.skip('login fail', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(selectorList.loginField).type('admin')
+    cy.get(selectorList.passwordField).type('admin')
+    cy.get(selectorList.loginButton).click()
+    cy.get(selectorList.loginFailAlert).should('be.visible')
   })
 })
